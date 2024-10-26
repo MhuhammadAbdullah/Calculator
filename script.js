1c3edf482dc46ed6bfc47a720d0a90eeb5d967ed
Muhammad Abdullah <abaloch0033@gmail.com>
2024-10-26 18:06:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+var display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    if (display.value !== '' && !isNaN(display.value.slice(-1))) {
+        display.value += operator;
+    }
+}
+
+function appendDot() {
+    if (!display.value.includes('.')) {
+        display.value += '.';
+    }
+}
+
+function calculateResult() {
+    if (display.value) {
+        display.value = eval(display.value.replace('x', '*').replace('%', '/100'));
+    }
+}
 
